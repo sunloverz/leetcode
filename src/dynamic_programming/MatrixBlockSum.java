f7c1d67272d02a32078540e8efe7cf1caf5319d9
sunloverz <sunloverz.kg@gmail.com>
2020-06-03 15:42:55 +0600

    Solve max sub array problem

--- src/dynamic_programming/MatrixBlockSum.java
+++ src/dynamic_programming/MatrixBlockSum.java
@@ -1,4 +1,30 @@
 package dynamic_programming;
 
 public class MatrixBlockSum {
+
+  public static int[][] matrixBlockSum(int[][] mat, int K) {
+    int m = mat.length, n = mat[0].length;
+    int[][] rangeSum = new int[m + 1][n + 1];
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        rangeSum[i + 1][j + 1] = rangeSum[i + 1][j] + rangeSum[i][j + 1] - rangeSum[i][j] + mat[i][j];
+    int[][] ans = new int[m][n];
+    for (int r = 0; r < m; r++) {
+      for (int c = 0; c < n; c++) {
+        int r1 = Math.max(0, r - K), c1 = Math.max(0, c - K);
+        int r2 = Math.min(m - 1, r + K), c2 = Math.min(n - 1, c + K);
+        r1++; c1++; r2++; c2++; // Since `sum` start with 1 so we need to increase r1, c1, r2, c2 by 1
+        ans[r][c] = rangeSum[r2][c2] - rangeSum[r2][c1-1] - rangeSum[r1-1][c2] + rangeSum[r1-1][c1-1];
+      }
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    int [][]mat = {{1,2,3},{4,5,6},{7,8,9}};
+    matrixBlockSum(mat, 1);
+
+  }
+
 }
+
--- src/dynamic_programming/MaximumSubarray.java
+++ src/dynamic_programming/MaximumSubarray.java
@@ -1,8 +1,23 @@
 package dynamic_programming;
 
 public class MaximumSubarray {
-  public int maxSubArray(int[] nums) {
-    return 0;
+  public static int maxSubArray(int[] nums) {
+    int len = nums.length;
+    if(len==1) return nums[0];
+    if(len==0) return Integer.MIN_VALUE;
+    int[] dp = new int[len];
+    dp[0] = nums[0];
+    int max = nums[0];
+    for(int i=1;i<len;i++) {
+      dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
+      max = Math.max(max, dp[i]);
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    int []mat = {1,2};
+    maxSubArray(mat);
   }
 }
-// [-2,1,-3,4,-1,2,1,-5,4]
+
