1f8208d7dc0295ea594acad42aa5c91e4f791c8b
sunloverz <sunloverz.kg@gmail.com>
2020-10-04 11:12:19 +0600

    Refactor longest palindrome

--- src/hash_table/LongestPalindrome.java
+++ src/hash_table/LongestPalindrome.java
@@ -2,18 +2,11 @@ package hash_table;
 
 public class LongestPalindrome {
   public int longestPalindrome(String s) {
-    int[] arr = new int[100];
-    s = s.toLowerCase();
-    for(char ch:s.toCharArray()) arr[ch-'a']++;
+    int[] arr = new int[128];
+    for(char ch:s.toCharArray()) arr[ch]++;
     int count = 0;
-    int max = Integer.MIN_VALUE;
-    for(int i=0;i<100;i++) {
-      if(arr[i]%2==0) {
-        count += arr[i];
-      } else {
-        max = Integer.max(max, arr[i]);
-      }
-    }
-    return count + (max == Integer.MIN_VALUE? 0: max);
+    for(int i=0; i<128; i++)
+      count += (arr[i] /2) * 2;
+    return (count == s.length()? count: count+1);
   }
 }
